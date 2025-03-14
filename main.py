7235130979b2d6b806886e349abbc05d21d12bac
tony007x <aekanan2004@gmail.com>
2025-03-14 22:39:04 +0700

    แก้ cors

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from tensorflow import keras
 import numpy as np
 import cv2
 
 
 app = FastAPI()
+
+#แก้ติดคอ
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
