e27c36de96613d9692cbb6ea0ec0bd527257872a
tony007x <aekanan2004@gmail.com>
2025-03-14 20:59:23 +0700

    .

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+@app.get("/hello")
+def hello():
+    return {"message": "dsadsasadadsdsas!@#@EDSADsdsa"}
